b3bd2a2ba1a617e434087936a76a9693bf8fa2b3
tcrct <tcrct@163.com>
2019-07-09 21:34:21 +0800

    持续开发

---
+++ src/main/java/com/duangframework/workflow/App.java
@@ -0,0 +1,35 @@
+package com.duangframework.workflow;
+
+import com.duangframework.kit.HttpKit;
+import com.duangframework.kit.PathKit;
+import com.duangframework.kit.ThreadPoolKit;
+import com.duangframework.net.http.HttpRequest;
+import com.duangframework.net.http.HttpResult;
+import com.duangframework.workflow.core.DuangEngine;
+import com.duangframework.workflow.core.ProcessDefinition;
+import com.duangframework.workflow.core.ProcessInstance;
+import com.duangframework.workflow.entity.WorkFlow;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.util.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        String path = PathKit.getWebRootPath() + "/src/main/resources/demo.xml";
+//        System.out.println(path);
+        try {
+            String xmlDoc = FileUtils.readFileToString(new File(path));
+            DuangEngine engine = new DuangEngine();
+            // 解释XML并返回流程定义对象
+            ProcessDefinition processDefinition = engine.parse(xmlDoc);
+            // 发布流程，得到进程实例
+            List<ProcessInstance> processInstance = engine.deploy(processDefinition);
+            // 运行进程实例
+//            WorkFlow workFlow = engine.execute(processInstance);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
